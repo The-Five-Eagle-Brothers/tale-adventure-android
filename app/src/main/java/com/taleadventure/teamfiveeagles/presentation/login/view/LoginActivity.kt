7e8b891d28dfe3856b18cf64ea6f5e37541117f7
giovannijunseokim <kingjunseo@gmail.com>
2023-07-26 00:44:23 +0900

    [feat]#3 kakao login init

--- app/src/main/java/com/taleadventure/teamfiveeagles/presentation/login/view/LoginActivity.kt
+++ app/src/main/java/com/taleadventure/teamfiveeagles/presentation/login/view/LoginActivity.kt
@@ -11,9 +11,49 @@ class LoginActivity : BaseDataBindingActivity<ActivityLoginBinding>(R.layout.act
     private val viewModel by viewModels<LoginViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun bindViewModelWithBinding() {
         binding.vm = viewModel
     }
+
+//    private fun setKakaoLoginBtnClickEvent() {
+//        val kakaoLoginCallback: (OAuthToken?, Throwable?) -> Unit = { socialToken, error ->
+//            if (error != null) {
+//                Timber.e(error)
+//            } else if (socialToken != null) {
+//                Timber.d(socialToken.accessToken)
+//            }
+//        }
+//
+//        if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+//            // 카카오톡 로그인
+//            UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+//                // 로그인 실패 부분
+//                if (error != null) {
+//                    // 사용자 취소
+//                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+//                        return@loginWithKakaoTalk
+//                    }
+//                    // 다른 오류 - 카톡으로 안 될 경우
+//                    else {
+//                        UserApiClient.instance.loginWithKakaoAccount(
+//                            this, callback = kakaoLoginCallback
+//                        ) // 카카오 이메일 로그인
+//                    }
+//                }
+//                // 로그인 성공 부분
+//                else if (token != null) {
+//                    kakaoLoginCallback.invoke(token, error)
+//                }
+//            }
+//        } else {
+//            Timber.d("카카오톡이 설치되어있지 않습니다.")
+//            // 카카오 이메일 로그인
+//            UserApiClient.instance.loginWithKakaoAccount(
+//                this, callback = kakaoLoginCallback
+//            )
+//        }
+//    }
 }
