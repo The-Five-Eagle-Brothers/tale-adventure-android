daf39326f1711fc32fdbc743982cd8b0778a9eb8
giovannijunseokim <kingjunseo@gmail.com>
2023-07-25 23:45:39 +0900

    [chore]#3 Kakao Sdk init

--- app/src/main/java/com/taleadventure/teamfiveeagles/TaleAdventureApp.kt
+++ app/src/main/java/com/taleadventure/teamfiveeagles/TaleAdventureApp.kt
@@ -1,6 +1,9 @@
 package com.taleadventure.teamfiveeagles
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import com.kakao.sdk.common.KakaoSdk
 import com.taleadventure.teamfiveeagles.util.timber.TaleAdventureTimber
 import timber.log.Timber
 
@@ -8,7 +11,13 @@ class TaleAdventureApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
         setTimber()
+        setKakaoSdk()
+    }
+
+    private fun setKakaoSdk() {
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
     }
 
     private fun setTimber() {
