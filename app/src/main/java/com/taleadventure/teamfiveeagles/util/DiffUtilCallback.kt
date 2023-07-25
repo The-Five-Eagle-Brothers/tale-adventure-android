356bff477611050adf704b7b4721bda9b12b6683
giovannijunseokim <kingjunseo@gmail.com>
2023-07-25 23:20:35 +0900

    [chore]#3 DiffUtilCallback refactor

--- app/src/main/java/com/taleadventure/teamfiveeagles/util/DiffUtilCallback.kt
+++ app/src/main/java/com/taleadventure/teamfiveeagles/util/DiffUtilCallback.kt
@@ -1,11 +1,9 @@
-package com.android.ssutudy.util
+package com.taleadventure.teamfiveeagles.util
 
 import androidx.recyclerview.widget.DiffUtil
 
 class DiffUtilCallback<T : Any> : DiffUtil.ItemCallback<T>() {
-    override fun areItemsTheSame(oldItem: T, newItem: T): Boolean =
-        oldItem === newItem
+    override fun areItemsTheSame(oldItem: T, newItem: T): Boolean = oldItem === newItem
 
-    override fun areContentsTheSame(oldItem: T, newItem: T): Boolean =
-        oldItem == newItem
+    override fun areContentsTheSame(oldItem: T, newItem: T): Boolean = oldItem == newItem
 }
