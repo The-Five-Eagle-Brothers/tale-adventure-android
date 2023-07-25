095888cada1bddf8e453c41a2ed39c98ecf6fb92
giovannijunseokim <kingjunseo@gmail.com>
2023-07-25 23:18:16 +0900

    [feat]#3 Timber Application 에서 init

--- app/src/main/java/com/taleadventure/teamfiveeagles/TaleAdventureApp.kt
+++ app/src/main/java/com/taleadventure/teamfiveeagles/TaleAdventureApp.kt
@@ -1,9 +1,19 @@
 package com.taleadventure.teamfiveeagles
 
 import android.app.Application
+import com.taleadventure.teamfiveeagles.util.timber.TaleAdventureTimber
+import timber.log.Timber
 
 class TaleAdventureApp : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        setTimber()
+    }
+
+    private fun setTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(TaleAdventureTimber())
+        }
     }
 }
