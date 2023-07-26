16a3919898bebae578b262e92d1906b1db10aa0c
giovannijunseokim <kingjunseo@gmail.com>
2023-07-27 01:04:16 +0900

    [ui]#7 activity_category에 fragmentContainerView 생성

--- app/src/main/java/com/taleadventure/teamfiveeagles/presentation/category/view/CategoryActivity.kt
+++ app/src/main/java/com/taleadventure/teamfiveeagles/presentation/category/view/CategoryActivity.kt
@@ -1,15 +1,10 @@
 package com.taleadventure.teamfiveeagles.presentation.category.view
 
-import android.os.Bundle
 import android.view.LayoutInflater
 import com.taleadventure.teamfiveeagles.databinding.ActivityCategoryBinding
 import com.taleadventure.teamfiveeagles.presentation.base.BaseViewBindingActivity
 
 class CategoryActivity : BaseViewBindingActivity<ActivityCategoryBinding>() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun setBinding(layoutInflater: LayoutInflater): ActivityCategoryBinding =
         ActivityCategoryBinding.inflate(layoutInflater)
