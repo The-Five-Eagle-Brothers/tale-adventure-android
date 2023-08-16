7a7ff6eb39f8e073c50b0d27c60cba9b09b9c018
giovannijunseokim <kingjunseo@gmail.com>
2023-08-16 17:06:16 +0900

    [feat]#10 initial commit

---
+++ TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.open6.taleadventure.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.open6.taleadventure.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ TaleAdventure/app/src/main/java/com/open6/taleadventure/util/extensions/BundleExtensions.kt
@@ -0,0 +1,11 @@
+package com.android.ssutudy.util.extensions
+
+import android.os.Build
+import android.os.Bundle
+
+fun <T> Bundle.getParcelableFromBundle(name: String, clazz: Class<T>): T? =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getParcelable(name, clazz)
+    } else {
+        getParcelable(name)
+    }
