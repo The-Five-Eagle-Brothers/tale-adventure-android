80426e575ae516c2b5eb6fb7b90fc0687b232092
giovannijunseokim <kingjunseo@gmail.com>
2023-08-24 15:55:07 +0900

    [feat]#16 나이대 하나만 선택하게 하는 로직 구현

--- TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/onboard/viewmodel/OnboardFirstViewModel.kt
+++ TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/onboard/viewmodel/OnboardFirstViewModel.kt
@@ -1,5 +1,44 @@
 package com.open6.taleadventure.presentation.onboard.viewmodel
 
+import android.widget.TextView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
+import timber.log.Timber
 
-class OnboardFirstViewModel : ViewModel() {}
+class OnboardFirstViewModel : ViewModel() {
+    private var ageMap: MutableMap<TextView, Int>? = null
+    fun setAgeMap(ageMap: MutableMap<TextView, Int>) {
+        this.ageMap = ageMap
+    }
+
+    private val selectedTextView: MutableLiveData<TextView?> = MutableLiveData(null)
+    private val selectedMaxAge: LiveData<Int?> = selectedTextView.map { textView ->
+        ageMap?.get(textView)
+    }
+    val isSelectedTextViewExist: LiveData<Boolean> = selectedTextView.map { textView ->
+        textView != null
+    }
+
+    fun setSelectedView(newTextView: TextView) {
+        if (selectedTextView.value == null) {
+            selectedTextView.value = newTextView
+            newTextView.isSelected = true
+            return
+        }
+
+        if (selectedTextView.value == newTextView) {
+            selectedTextView.value = null
+            newTextView.isSelected = false
+            Timber.e(selectedTextView.value.toString())
+            return
+        }
+
+        selectedTextView.value?.isSelected = false
+        newTextView.isSelected = true
+        selectedTextView.value = newTextView
+
+        Timber.e(selectedTextView.value.toString())
+    }
+}
