46ee0ed3bb8d42d10babfd7623950abe5a19ece2
giovannijunseokim <kingjunseo@gmail.com>
2023-08-25 19:23:04 +0900

    [feat]#16 조건에 따라 완료 버튼 활성화 기능 구현

--- TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/onboard/viewmodel/OnboardFourthViewModel.kt
+++ TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/onboard/viewmodel/OnboardFourthViewModel.kt
@@ -1,5 +1,11 @@
 package com.open6.taleadventure.presentation.onboard.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 
-class OnboardFourthViewModel : ViewModel() {}
+class OnboardFourthViewModel : ViewModel() {
+    val nickname: MutableLiveData<String> = MutableLiveData("")
+    val isNickNameValid: LiveData<Boolean> = nickname.map { nickname -> nickname.length in 2..8 }
+}
