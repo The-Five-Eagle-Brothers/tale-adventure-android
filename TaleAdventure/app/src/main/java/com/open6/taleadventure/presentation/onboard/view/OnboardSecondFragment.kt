04b56d19f5cbf68295834416d514b2ac737d8aea
giovannijunseokim <kingjunseo@gmail.com>
2023-08-25 17:47:03 +0900

    [feat]#16 온보딩 2번째 페이지 기능 구현

--- TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/onboard/view/OnboardSecondFragment.kt
+++ TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/onboard/view/OnboardSecondFragment.kt
@@ -3,6 +3,7 @@ package com.open6.taleadventure.presentation.onboard.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.open6.taleadventure.R
 import com.open6.taleadventure.databinding.FragmentOnboardSecondBinding
 import com.open6.taleadventure.presentation.base.BaseDataBindingFragment
@@ -10,7 +11,7 @@ import com.open6.taleadventure.presentation.onboard.viewmodel.OnboardSecondViewM
 
 class OnboardSecondFragment :
     BaseDataBindingFragment<FragmentOnboardSecondBinding>(R.layout.fragment_onboard_second) {
-
+    private val navController by lazy { findNavController() }
     private val viewModel by viewModels<OnboardSecondViewModel>()
 
     override fun bindViewModelWithBinding() {
@@ -20,6 +21,25 @@ class OnboardSecondFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setClickEvents()
+    }
+
+    private fun setClickEvents() {
+        setNextTVClickEvent()
+        setBackIVClickEvent()
+    }
+
+    private fun setBackIVClickEvent() {
+        binding.ivOnboardSecondBack.setOnClickListener {
+            navController.navigateUp()
+        }
+    }
+
+    private fun setNextTVClickEvent() {
+        binding.tvOnboardSecondNext.setOnClickListener {
+            navController.navigate(R.id.action_onboardSecondFragment_to_onboardThirdFragment)
+            viewModel.resetSelectedViews()
+        }
     }
 
 }
