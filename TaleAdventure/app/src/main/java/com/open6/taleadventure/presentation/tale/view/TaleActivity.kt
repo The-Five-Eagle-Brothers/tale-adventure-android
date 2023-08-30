5079c8f2340c9406354b184580624557d61f7114
giovannijunseokim <kingjunseo@gmail.com>
2023-08-30 16:44:30 +0900

    [feat]#28 ChapterAdapter 연결

--- TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/tale/view/TaleActivity.kt
+++ TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/tale/view/TaleActivity.kt
@@ -2,14 +2,23 @@ package com.open6.taleadventure.presentation.tale.view
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import com.open6.taleadventure.R
 import com.open6.taleadventure.databinding.ActivityTaleBinding
 import com.open6.taleadventure.presentation.base.BaseViewBindingActivity
+import com.open6.taleadventure.presentation.tale.adapter.ChapterAdapter
 
 class TaleActivity : BaseViewBindingActivity<ActivityTaleBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setViews()
+    }
+
+    private fun setViews() {
+        setChapterAdapter()
+    }
+
+    private fun setChapterAdapter() {
+        binding.rvTaleChapter.adapter = ChapterAdapter()
     }
 
     override fun setBinding(inflater: LayoutInflater): ActivityTaleBinding =
