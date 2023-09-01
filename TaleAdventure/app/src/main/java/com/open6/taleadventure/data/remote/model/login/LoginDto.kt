494262bf63e103e4312f1d1af3a1d907606e8b12
giovannijunseokim <kingjunseo@gmail.com>
2023-09-01 13:56:41 +0900

    [feat]#27 LoginDto 수정

--- TaleAdventure/app/src/main/java/com/open6/taleadventure/data/remote/model/login/LoginDto.kt
+++ TaleAdventure/app/src/main/java/com/open6/taleadventure/data/remote/model/login/LoginDto.kt
@@ -1,14 +1,20 @@
 package com.open6.taleadventure.data.remote.model.login
 
+import com.open6.taleadventure.data.remote.model.wrapper.ResponseWrapper
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class RequestLoginDto(
-    @SerialName("token") val token: String,
+    @SerialName("accessToken") val accessToken: String,
 )
 
 @Serializable
 data class ResponseLoginDto(
-    @SerialName("data") val data: String,
-)
+    @SerialName("data") val data: ResponseWrapper<AppToken>,
+) {
+    @Serializable
+    data class AppToken(
+        @SerialName("appToken") val appToken: String,
+    )
+}
