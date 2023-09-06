6df2f00a77d879a4794a513a16c9dea240909014
giovannijunseokim <kingjunseo@gmail.com>
2023-09-07 06:15:37 +0900

    [feat]#61 get 서버통신 로직 반영

--- TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/end/adapter/WordsAdapter.kt
+++ TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/end/adapter/WordsAdapter.kt
@@ -4,29 +4,41 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.open6.taleadventure.data.remote.model.word.ResponseGameWordsDto
 import com.open6.taleadventure.databinding.ItemGameScoreBinding
-import com.open6.taleadventure.presentation.end.model.Word
 import com.open6.taleadventure.util.DiffUtilCallback
 
-class WordsAdapter : ListAdapter<Word, WordsAdapter.WordsViewHolder>(DiffUtilCallback<Word>()) {
+class WordsAdapter :
+    ListAdapter<ResponseGameWordsDto, WordsAdapter.WordsViewHolder>(DiffUtilCallback<ResponseGameWordsDto>()) {
+    private var onItemClick: ((Int, Boolean) -> Unit)? = null
+    fun setOnItemClick(onClick: (Int, Boolean) -> Unit) {
+        onItemClick = onClick
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordsViewHolder {
         val binding =
             ItemGameScoreBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return WordsViewHolder(binding)
+        return WordsViewHolder(binding, onItemClick)
     }
 
-    class WordsViewHolder(val binding: ItemGameScoreBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        fun onBind(itemList: List<Word>, position: Int) {
+    class WordsViewHolder(
+        val binding: ItemGameScoreBinding,
+        private val onClick: ((Int, Boolean) -> Unit)?,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(itemList: List<ResponseGameWordsDto>, position: Int) {
             setViews(itemList[position])
+            setClickEvents(position, binding.ivItemGameScoreBookmark.isSelected)
         }
 
-        private fun setViews(word: Word) {
+        private fun setClickEvents(position: Int, currentlyBookmarked: Boolean) {
+            onClick?.invoke(position, !currentlyBookmarked)
+        }
+
+        private fun setViews(word: ResponseGameWordsDto) {
             with(binding) {
-                tvItemGameScoreWord.text = word.title
-                tvItemGameScoreDesc.text = word.desc
-                ivItemGameScoreBookmark.isSelected = word.isBookmarked
+                tvItemGameScoreWord.text = word.name
+                tvItemGameScoreDesc.text = word.mean
+                ivItemGameScoreBookmark.isSelected = word.bookMark
             }
         }
     }
--- TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/end/view/EndActivity.kt
+++ TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/end/view/EndActivity.kt
@@ -7,14 +7,49 @@ import com.open6.taleadventure.databinding.ActivityEndBinding
 import com.open6.taleadventure.presentation.base.activity.BaseDataBindingActivity
 import com.open6.taleadventure.presentation.end.adapter.WordsAdapter
 import com.open6.taleadventure.presentation.end.viewmodel.EndViewModel
+import com.open6.taleadventure.util.PublicString.CHAPTER_NAME
+import com.open6.taleadventure.util.PublicString.TALE_NAME
+import com.open6.taleadventure.util.extensions.makeToastMessage
 
 class EndActivity : BaseDataBindingActivity<ActivityEndBinding>(R.layout.activity_end) {
     private val viewModel by viewModels<EndViewModel>()
+    private val wordsAdapter by lazy { WordsAdapter() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        getData()
         setViews()
         setClickEvents()
+        setObservers()
+    }
+
+    private fun setObservers() {
+        setGameWordObservers()
+        setErrorObservers()
+    }
+
+    private fun setErrorObservers() {
+        viewModel.errorResponse.observe(this) { errorMessage ->
+            makeToastMessage(errorMessage)
+        }
+    }
+
+    private fun setGameWordObservers() {
+        viewModel.gameWords.observe(this) { successData ->
+            successData?.map {
+                viewModel.isBookmarkedList.add(element = it.bookMark)
+            }
+            wordsAdapter.submitList(successData)
+        }
+    }
+
+    private fun getData() {
+        val chapterName = intent.getStringExtra(CHAPTER_NAME)
+        if (!chapterName.isNullOrBlank()) {
+            viewModel.getChapterWords(chapterName)
+        } else {
+            intent.getStringExtra(TALE_NAME)?.let { viewModel.getMyWords(it) }
+        }
     }
 
     private fun setClickEvents() {
@@ -32,8 +67,14 @@ class EndActivity : BaseDataBindingActivity<ActivityEndBinding>(R.layout.activit
     }
 
     private fun setScoreAdapter() {
-        binding.rvEndWords.adapter = WordsAdapter()
+        binding.rvEndWords.adapter = wordsAdapter
+        wordsAdapter.setOnItemClick(changeIsBookmarked)
     }
+
+    private val changeIsBookmarked: (position: Int, isBookmarked: Boolean) -> Unit =
+        { position, isBookmarked ->
+            viewModel.isBookmarkedList[position] = isBookmarked
+        }
 
     override fun bindViewModelWithBinding() {
         binding.vm = viewModel
