78f8b7acd79986ef50fd58dcef1a620ac34b6405
giovannijunseokim <kingjunseo@gmail.com>
2023-09-07 02:57:44 +0900

    [feat]#54 서버통신 로직 구현

--- TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/tale/viewmodel/TaleViewModel.kt
+++ TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/tale/viewmodel/TaleViewModel.kt
@@ -1,6 +1,31 @@
 package com.open6.taleadventure.presentation.tale.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.open6.taleadventure.data.remote.api.ApiFactory.ServicePool.chapterService
+import com.open6.taleadventure.data.remote.model.chapter.ResponseChapterDto
+import com.open6.taleadventure.util.extensions.getErrorMessage
+import kotlinx.coroutines.launch
 
 class TaleViewModel : ViewModel() {
+
+    private val _getChaptersSuccessResponse = MutableLiveData<List<ResponseChapterDto>?>()
+    val getChaptersSuccessResponse: LiveData<List<ResponseChapterDto>?> =
+        _getChaptersSuccessResponse
+
+    private val _getChaptersErrorResponse = MutableLiveData<String>()
+    val getChaptersErrorResponse: LiveData<String> = _getChaptersErrorResponse
+
+    fun getChapters() {
+        viewModelScope.launch {
+            kotlin.runCatching { chapterService.getChapters() }
+                .fold(onSuccess = { successResponse ->
+                    _getChaptersSuccessResponse.value = successResponse.data
+                }, onFailure = { errorResponse ->
+                    _getChaptersErrorResponse.value = errorResponse.getErrorMessage()
+                })
+        }
+    }
 }
