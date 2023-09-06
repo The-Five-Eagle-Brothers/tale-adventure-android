2f364d8a36faa1409662214c6185e4e9de0b850b
giovannijunseokim <kingjunseo@gmail.com>
2023-09-07 02:57:58 +0900

    [feat]#54 서버통신 결과 반영 구현

--- TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/tale/view/TaleActivity.kt
+++ TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/tale/view/TaleActivity.kt
@@ -1,17 +1,65 @@
 package com.open6.taleadventure.presentation.tale.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.activity.viewModels
+import coil.load
+import com.open6.taleadventure.data.remote.model.chapter.ResponseChapterDto
 import com.open6.taleadventure.databinding.ActivityTaleBinding
 import com.open6.taleadventure.presentation.base.activity.BaseViewBindingActivity
 import com.open6.taleadventure.presentation.tale.adapter.ChapterAdapter
+import com.open6.taleadventure.presentation.tale.viewmodel.TaleViewModel
+import com.open6.taleadventure.presentation.wordgame.view.WordGameActivity
+import com.open6.taleadventure.util.PublicString.TALE_NAME
+import com.open6.taleadventure.util.extensions.makeToastMessage
 
 class TaleActivity : BaseViewBindingActivity<ActivityTaleBinding>() {
+    private val viewModel by viewModels<TaleViewModel>()
+    private val chapterAdapter by lazy { ChapterAdapter() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        getData()
         setViews()
         setClickEvents()
+        setObservers()
+    }
+
+    private fun setObservers() {
+        setGetChaptersObservers()
+    }
+
+    private fun setGetChaptersObservers() {
+        setGetChaptersSuccessObserver()
+        setGetChaptersErrorObserver()
+    }
+
+    private fun setGetChaptersSuccessObserver() {
+        viewModel.getChaptersSuccessResponse.observe(this) { successData ->
+            successData.run {
+                setData()
+                updateChapterAdapter()
+            }
+        }
+    }
+
+    private fun List<ResponseChapterDto>?.setData() {
+        binding.ivTaleBack.load(this?.first()?.taleBook?.chapterImageUrl)
+    }
+
+    private fun List<ResponseChapterDto>?.updateChapterAdapter() {
+        chapterAdapter.submitList(this)
+    }
+
+    private fun setGetChaptersErrorObserver() {
+        viewModel.getChaptersErrorResponse.observe(this) { errorMessage ->
+            makeToastMessage(errorMessage)
+        }
+    }
+
+    private fun getData() {
+        viewModel.getChapters()
     }
 
     private fun setClickEvents() {
@@ -27,7 +75,15 @@ class TaleActivity : BaseViewBindingActivity<ActivityTaleBinding>() {
     }
 
     private fun setChapterAdapter() {
-        binding.rvTaleChapter.adapter = ChapterAdapter()
+        binding.rvTaleChapter.adapter = chapterAdapter.also { adapter ->
+            adapter.setOnItemClick(navigateToGameActivity)
+        }
+    }
+
+    private val navigateToGameActivity: (String) -> Unit = { taleName ->
+        Intent(this, WordGameActivity::class.java).putExtra(TALE_NAME, taleName).also { intent ->
+            startActivity(intent)
+        }
     }
 
     override fun setBinding(inflater: LayoutInflater): ActivityTaleBinding =
