ab2d8c197c14f134a1b88a94933562ce38c50029
giovannijunseokim <kingjunseo@gmail.com>
2023-09-06 22:33:40 +0900

    [feat]#47 Home 서버통신 로직 구현

--- TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/home/viewmodel/HomeViewModel.kt
+++ TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/home/viewmodel/HomeViewModel.kt
@@ -1,6 +1,31 @@
 package com.open6.taleadventure.presentation.home.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.open6.taleadventure.data.remote.api.ApiFactory.ServicePool.homeService
+import com.open6.taleadventure.data.remote.model.home.ResponseHomeDto
+import com.open6.taleadventure.util.extensions.getErrorMessage
+import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
+
+    private val _getHomeDataSuccessResponse = MutableLiveData<ResponseHomeDto?>()
+    val getHomeDataSuccessResponse: LiveData<ResponseHomeDto?> = _getHomeDataSuccessResponse
+
+    private val _getHomeDataErrorResponse = MutableLiveData<String>()
+    val getHomeDataErrorResponse: LiveData<String> = _getHomeDataErrorResponse
+
+    fun getHomeData() {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                homeService.getHomeData()
+            }.fold(onSuccess = { successResponse ->
+                _getHomeDataSuccessResponse.value = successResponse.data
+            }, onFailure = { errorResponse ->
+                _getHomeDataErrorResponse.value = errorResponse.getErrorMessage()
+            })
+        }
+    }
 }
