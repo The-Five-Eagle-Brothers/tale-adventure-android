8aa69d24f4a5f1affd5718b63f842372aa38ec58
giovannijunseokim <kingjunseo@gmail.com>
2023-09-07 09:09:53 +0900

    [feat]#63 쿼리 추가

--- TaleAdventure/app/src/main/java/com/open6/taleadventure/data/remote/service/ChapterService.kt
+++ TaleAdventure/app/src/main/java/com/open6/taleadventure/data/remote/service/ChapterService.kt
@@ -4,8 +4,9 @@ import com.open6.taleadventure.data.remote.model.chapter.ResponseChapterDto
 import com.open6.taleadventure.data.remote.model.wrapper.ResponseWrapper
 import com.open6.taleadventure.util.PublicString.GET_CHAPTERS_PATH
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ChapterService {
     @GET(GET_CHAPTERS_PATH)
-    suspend fun getChapters(): ResponseWrapper<List<ResponseChapterDto>>
+    suspend fun getChapters(@Query("name") taleName: String): ResponseWrapper<List<ResponseChapterDto>>
 }
