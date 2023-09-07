04f4fd557960279f4c9be611507f81ee33513f48
giovannijunseokim <kingjunseo@gmail.com>
2023-09-07 09:30:09 +0900

    [feat]#63 onDismiss 기본값 null

--- TaleAdventure/app/src/main/java/com/open6/taleadventure/util/extensions/ViewExtensions.kt
+++ TaleAdventure/app/src/main/java/com/open6/taleadventure/util/extensions/ViewExtensions.kt
@@ -3,6 +3,10 @@ package com.open6.taleadventure.util.extensions
 import android.view.View
 import com.open6.taleadventure.view.GradeSnackbar
 
-fun View.makeGradeSnackbar(isCorrect: Boolean, anchorView: View? = null, onDismiss: () -> Unit) {
-    GradeSnackbar(this, isCorrect, anchorView = anchorView, onDismissed = onDismiss).show()
+fun View.makeGradeSnackbar(
+    isCorrect: Boolean,
+    anchorView: View? = null,
+    onDismiss: (() -> Unit)? = null,
+) {
+    GradeSnackbar(this, isCorrect, anchorView = anchorView, onDismiss = onDismiss).show()
 }
