9e36e61e3f83e4db09da28627365896568b4f964
giovannijunseokim <kingjunseo@gmail.com>
2023-09-10 20:58:42 +0900

    [fix]#63 구름 양쪽으로 배치 안 되던 버그 수정

--- TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/dailyadventure/list/view/DailyAdventureListActivity.kt
+++ TaleAdventure/app/src/main/java/com/open6/taleadventure/presentation/dailyadventure/list/view/DailyAdventureListActivity.kt
@@ -3,11 +3,15 @@ package com.open6.taleadventure.presentation.dailyadventure.list.view
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import com.open6.taleadventure.R
 import com.open6.taleadventure.databinding.ActivityDailyAdventureListBinding
 import com.open6.taleadventure.presentation.base.activity.BaseViewBindingActivity
 import com.open6.taleadventure.presentation.dailyadventure.game.view.DailyAdventureGameActivity
 import com.open6.taleadventure.presentation.dailyadventure.list.adapter.DailyAdventureListAdapter
 import com.open6.taleadventure.presentation.dailyadventure.list.model.DailyAdventure
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class DailyAdventureListActivity : BaseViewBindingActivity<ActivityDailyAdventureListBinding>() {
     private val dailyAdventureListAdapter by lazy {
@@ -44,11 +48,27 @@ class DailyAdventureListActivity : BaseViewBindingActivity<ActivityDailyAdventur
 
     private fun setDailyAdventureListAdapter() {
         binding.rvDailyAdventureList.adapter = dailyAdventureListAdapter
-        dailyAdventureListAdapter.submitList(listOf(DailyAdventure(1)))
+        dailyAdventureListAdapter.submitList(
+            listOf(
+                DailyAdventure(
+                    image = R.drawable.img_cloud_example, day = 1
+                )
+            )
+        )
     }
 
     private val navigateToDailyAdventureGameActivity: () -> Unit = {
         startActivity(Intent(this, DailyAdventureGameActivity::class.java))
+        GlobalScope.launch {
+            delay(1000) // 1초 대기
+            dailyAdventureListAdapter.submitList(
+                listOf(
+                    DailyAdventure(
+                        image = R.drawable.img_cloud_cleared_example, day = 1
+                    ), DailyAdventure(image = R.drawable.img_cloud_example, day = 2)
+                )
+            )
+        }
     }
 
     override fun setBinding(inflater: LayoutInflater): ActivityDailyAdventureListBinding =
