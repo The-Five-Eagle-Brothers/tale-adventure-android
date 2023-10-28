aba3d29f31e114c03fa7cb9225bd28a322275aa5
giovannijunseokim <kingjunseo@gmail.com>
2023-10-29 02:19:51 +0900

    [feat]#66 온보딩 체크 서비스 추가

--- TaleAdventure/app/src/main/java/com/open6/taleadventure/data/remote/service/LoginService.kt
+++ TaleAdventure/app/src/main/java/com/open6/taleadventure/data/remote/service/LoginService.kt
@@ -3,8 +3,12 @@ package com.open6.taleadventure.data.remote.service
 import com.open6.taleadventure.data.remote.model.login.RequestLoginDto
 import com.open6.taleadventure.data.remote.model.login.ResponseLoginDto
 import com.open6.taleadventure.data.remote.model.wrapper.ResponseWrapper
+import com.open6.taleadventure.util.PublicString.CHECK_USER_INFO_VALID_PATH
 import com.open6.taleadventure.util.PublicString.LOGIN_PATH
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface LoginService {
@@ -12,4 +16,14 @@ interface LoginService {
     suspend fun loginWithKakao(
         @Body request: RequestLoginDto,
     ): ResponseWrapper<ResponseLoginDto>?
+
+    @GET(CHECK_USER_INFO_VALID_PATH)
+    suspend fun checkUserInfoValid(): ResponseCheckUserInfoValidDto
 }
+
+@Serializable
+data class ResponseCheckUserInfoValidDto(
+    @SerialName("statusCode") val statusCode: String,
+    @SerialName("message") val message: String,
+    @SerialName("data") val data: Int?,
+)
